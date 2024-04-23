88b419ac95f8c3d2a418e0e4e27db00bbeda9203
Erick Lemos <ericklemos.dev@gmail.com>
2024-04-23 14:30:52 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+function saltarBtn0(){
+    botao = document.querySelector('.btn00');
+    botao.style.transform = 'translateY(-10px)';
+}
+
+function naoSaltoBtn0(){
+    botao = document.querySelector('.btn00');
+    botao.style.transform = 'translateY(0)';
+}
+
+function saltarBtn01(){
+    botao = document.querySelector('.btn01');
+    botao.style.transform = 'translateY(-10px)';
+}
+
+function naoSaltoBtn01(){
+    botao = document.querySelector('.btn01');
+    botao.style.transform = 'translateY(0)';
+}
+
+function saltarBtn02(){
+    botao = document.querySelector('.btn02');
+    botao.style.transform = 'translateY(-10px)';
+}
+
+function naoSaltoBtn02(){
+    botao = document.querySelector('.btn02');
+    botao.style.transform = 'translateY(0)';
+}
+
+function saltarBtn03(){
+    botao = document.querySelector('.btn03');
+    botao.style.transform = 'translateY(-10px)';
+}
+
+function naoSaltoBtn03(){
+    botao = document.querySelector('.btn03');
+    botao.style.transform = 'translateY(0)';
+}
